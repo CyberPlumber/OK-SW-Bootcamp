3ae73b2f11e7a2b56a1597b85ff77d5fcbc74a55
Jay Liang <jay.j.liang@gmail.com>
2013-07-01 14:29:43 -0500

    Added module with functions

--- add_numb.py
+++ add_numb.py
@@ -1,7 +1,9 @@
 """ This short program just adds numbers. """
-def add_two(a,b):
-   return a+b
+from adding import add_two, add_three
+
 def main():
-   print ("2 + 3 = ",add_two(2,3))
+   print ("2+2+2 = ", add_three(2,2,2))
+   print ("2 + 3 = ", add_two(2,3))
+
 if __name__ == "__main__":
      main()
